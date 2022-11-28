ee7a7d015d483a77ba6beb0fc0ac457b254d4278
Charis02 <chge2002@gmail.com>
2022-11-27 19:38:57 -0500

    use main clk to avoid clock domain crossing

--- src/sd_controller.sv
+++ src/sd_controller.sv
@@ -31,6 +31,8 @@ module sd_controller(
     output ready, // HIGH if the SD card is ready for a read or write operation.
     input [31:0] address,   // Memory address for read/write operation. This MUST 
                             // be a multiple of 512 bytes, due to SD sectoring.
+
+    input real_clk,
     input clk,  // 25 MHz clock.
     output [4:0] status // For debug purposes: Current state of controller.
 );
@@ -69,206 +71,212 @@ module sd_controller(
     
     reg [9:0] byte_counter;
     reg [9:0] bit_counter;
+
+    reg clk_prev;
+    wire clk_posedge = (clk&~clk_prev);
     
     reg [26:0] boot_counter = 27'd100_000_000;
-    always @(posedge clk) begin
-        if(reset == 1) begin
-            state <= RST;
-            sclk_sig <= 0;
-            boot_counter <= 27'd100_000_000;
-        end
-        else begin
-            case(state)
-                RST: begin
-                    if(boot_counter == 0) begin
-                        sclk_sig <= 0;
-                        cmd_out <= {56{1'b1}};
-                        byte_counter <= 0;
-                        byte_available <= 0;
-                        ready_for_next_byte <= 0;
-                        cmd_mode <= 1;
-                        bit_counter <= 160;
-                        cs <= 1;
-                        state <= INIT;
+    always @(posedge real_clk) begin
+        clk_prev <= clk;
+        if (clk_posedge) begin
+            if(reset == 1) begin
+                state <= RST;
+                sclk_sig <= 0;
+                boot_counter <= 27'd100_000_000;
+            end
+            else begin
+                case(state)
+                    RST: begin
+                        if(boot_counter == 0) begin
+                            sclk_sig <= 0;
+                            cmd_out <= {56{1'b1}};
+                            byte_counter <= 0;
+                            byte_available <= 0;
+                            ready_for_next_byte <= 0;
+                            cmd_mode <= 1;
+                            bit_counter <= 160;
+                            cs <= 1;
+                            state <= INIT;
+                        end
+                        else begin
+                            boot_counter <= boot_counter - 1;
+                        end
                     end
-                    else begin
-                        boot_counter <= boot_counter - 1;
-                    end
-                end
-                INIT: begin
-                    if(bit_counter == 0) begin
-                        cs <= 0;
-                        state <= CMD0;
-                    end
-                    else begin
-                        bit_counter <= bit_counter - 1;
-                        sclk_sig <= ~sclk_sig;
-                    end
-                end
-                CMD0: begin
-                    cmd_out <= 56'hFF_40_00_00_00_00_95;
-                    bit_counter <= 55;
-                    return_state <= CMD55;
-                    state <= SEND_CMD;
-                end
-                CMD55: begin
-                    cmd_out <= 56'hFF_77_00_00_00_00_01;
-                    bit_counter <= 55;
-                    return_state <= CMD41;
-                    state <= SEND_CMD;
-                end
-                CMD41: begin
-                    cmd_out <= 56'hFF_69_00_00_00_00_01;
-                    bit_counter <= 55;
-                    return_state <= POLL_CMD;
-                    state <= SEND_CMD;
-                end
-                POLL_CMD: begin
-                    if(recv_data[0] == 0) begin
-                        state <= IDLE;
-                    end
-                    else begin
-                        state <= CMD55;
-                    end
-                end
-                IDLE: begin
-                    if(rd == 1) begin
-                        state <= READ_BLOCK;
-                    end
-                    else if(wr == 1) begin
-                        state <= WRITE_BLOCK_CMD;
-                    end
-                    else begin
-                        state <= IDLE;
-                    end
-                end
-                READ_BLOCK: begin
-                    cmd_out <= {16'hFF_51, address, 8'hFF};
-                    bit_counter <= 55;
-                    return_state <= READ_BLOCK_WAIT;
-                    state <= SEND_CMD;
-                end
-                READ_BLOCK_WAIT: begin
-                    if(sclk_sig == 1 && miso == 0) begin
-                        byte_counter <= 511;
-                        bit_counter <= 7;
-                        return_state <= READ_BLOCK_DATA;
-                        state <= RECEIVE_BYTE;
-                    end
-                    sclk_sig <= ~sclk_sig;
-                end
-                READ_BLOCK_DATA: begin
-                    dout <= recv_data;
-                    byte_available <= 1;
-                    if (byte_counter == 0) begin
-                        bit_counter <= 7;
-                        return_state <= READ_BLOCK_CRC;
-                        state <= RECEIVE_BYTE;
-                    end
-                    else begin
-                        byte_counter <= byte_counter - 1;
-                        return_state <= READ_BLOCK_DATA;
-                        bit_counter <= 7;
-                        state <= RECEIVE_BYTE;
-                    end
-                end
-                READ_BLOCK_CRC: begin
-                    bit_counter <= 7;
-                    return_state <= IDLE;
-                    state <= RECEIVE_BYTE;
-                end
-                SEND_CMD: begin
-                    if (sclk_sig == 1) begin
-                        if (bit_counter == 0) begin
-                            state <= RECEIVE_BYTE_WAIT;
+                    INIT: begin
+                        if(bit_counter == 0) begin
+                            cs <= 0;
+                            state <= CMD0;
                         end
                         else begin
                             bit_counter <= bit_counter - 1;
-                            cmd_out <= {cmd_out[54:0], 1'b1};
+                            sclk_sig <= ~sclk_sig;
                         end
                     end
-                    sclk_sig <= ~sclk_sig;
-                end
-                RECEIVE_BYTE_WAIT: begin
-                    if (sclk_sig == 1) begin
-                        if (miso == 0) begin
-                            recv_data <= 0;
-                            bit_counter <= 6;
+                    CMD0: begin
+                        cmd_out <= 56'hFF_40_00_00_00_00_95;
+                        bit_counter <= 55;
+                        return_state <= CMD55;
+                        state <= SEND_CMD;
+                    end
+                    CMD55: begin
+                        cmd_out <= 56'hFF_77_00_00_00_00_01;
+                        bit_counter <= 55;
+                        return_state <= CMD41;
+                        state <= SEND_CMD;
+                    end
+                    CMD41: begin
+                        cmd_out <= 56'hFF_69_00_00_00_00_01;
+                        bit_counter <= 55;
+                        return_state <= POLL_CMD;
+                        state <= SEND_CMD;
+                    end
+                    POLL_CMD: begin
+                        if(recv_data[0] == 0) begin
+                            state <= IDLE;
+                        end
+                        else begin
+                            state <= CMD55;
+                        end
+                    end
+                    IDLE: begin
+                        if(rd == 1) begin
+                            state <= READ_BLOCK;
+                        end
+                        else if(wr == 1) begin
+                            state <= WRITE_BLOCK_CMD;
+                        end
+                        else begin
+                            state <= IDLE;
+                        end
+                    end
+                    READ_BLOCK: begin
+                        cmd_out <= {16'hFF_51, address, 8'hFF};
+                        bit_counter <= 55;
+                        return_state <= READ_BLOCK_WAIT;
+                        state <= SEND_CMD;
+                    end
+                    READ_BLOCK_WAIT: begin
+                        if(sclk_sig == 1 && miso == 0) begin
+                            byte_counter <= 511;
+                            bit_counter <= 7;
+                            return_state <= READ_BLOCK_DATA;
+                            state <= RECEIVE_BYTE;
+                        end
+                        sclk_sig <= ~sclk_sig;
+                    end
+                    READ_BLOCK_DATA: begin
+                        dout <= recv_data;
+                        byte_available <= 1;
+                        if (byte_counter == 0) begin
+                            bit_counter <= 7;
+                            return_state <= READ_BLOCK_CRC;
+                            state <= RECEIVE_BYTE;
+                        end
+                        else begin
+                            byte_counter <= byte_counter - 1;
+                            return_state <= READ_BLOCK_DATA;
+                            bit_counter <= 7;
                             state <= RECEIVE_BYTE;
                         end
                     end
-                    sclk_sig <= ~sclk_sig;
-                end
-                RECEIVE_BYTE: begin
-                    byte_available <= 0;
-                    if (sclk_sig == 1) begin
-                        recv_data <= {recv_data[6:0], miso};
-                        if (bit_counter == 0) begin
-                            state <= return_state;
-                        end
-                        else begin
-                            bit_counter <= bit_counter - 1;
-                        end
-                    end
-                    sclk_sig <= ~sclk_sig;
-                end
-                WRITE_BLOCK_CMD: begin
-                    cmd_out <= {16'hFF_58, address, 8'hFF};
-                    bit_counter <= 55;
-                    return_state <= WRITE_BLOCK_INIT;
-                    state <= SEND_CMD;
-		    ready_for_next_byte <= 1;
-                end
-                WRITE_BLOCK_INIT: begin
-                    cmd_mode <= 0;
-                    byte_counter <= WRITE_DATA_SIZE; 
-                    state <= WRITE_BLOCK_DATA;
-                    ready_for_next_byte <= 0;
-                end
-                WRITE_BLOCK_DATA: begin
-                    if (byte_counter == 0) begin
-                        state <= RECEIVE_BYTE_WAIT;
-                        return_state <= WRITE_BLOCK_WAIT;
-                    end
-                    else begin
-                        if ((byte_counter == 2) || (byte_counter == 1)) begin
-                            data_sig <= 8'hFF;
-                        end
-                        else if (byte_counter == WRITE_DATA_SIZE) begin
-                            data_sig <= 8'hFE;
-                        end
-                        else begin
-                            data_sig <= din;
-                            ready_for_next_byte <= 1;
-                        end
+                    READ_BLOCK_CRC: begin
                         bit_counter <= 7;
-                        state <= WRITE_BLOCK_BYTE;
-                        byte_counter <= byte_counter - 1;
+                        return_state <= IDLE;
+                        state <= RECEIVE_BYTE;
                     end
-                end
-                WRITE_BLOCK_BYTE: begin
-                    if (sclk_sig == 1) begin
-                        if (bit_counter == 0) begin
-                            state <= WRITE_BLOCK_DATA;
-                            ready_for_next_byte <= 0;
+                    SEND_CMD: begin
+                        if (sclk_sig == 1) begin
+                            if (bit_counter == 0) begin
+                                state <= RECEIVE_BYTE_WAIT;
+                            end
+                            else begin
+                                bit_counter <= bit_counter - 1;
+                                cmd_out <= {cmd_out[54:0], 1'b1};
+                            end
+                        end
+                        sclk_sig <= ~sclk_sig;
+                    end
+                    RECEIVE_BYTE_WAIT: begin
+                        if (sclk_sig == 1) begin
+                            if (miso == 0) begin
+                                recv_data <= 0;
+                                bit_counter <= 6;
+                                state <= RECEIVE_BYTE;
+                            end
+                        end
+                        sclk_sig <= ~sclk_sig;
+                    end
+                    RECEIVE_BYTE: begin
+                        byte_available <= 0;
+                        if (sclk_sig == 1) begin
+                            recv_data <= {recv_data[6:0], miso};
+                            if (bit_counter == 0) begin
+                                state <= return_state;
+                            end
+                            else begin
+                                bit_counter <= bit_counter - 1;
+                            end
+                        end
+                        sclk_sig <= ~sclk_sig;
+                    end
+                    WRITE_BLOCK_CMD: begin
+                        cmd_out <= {16'hFF_58, address, 8'hFF};
+                        bit_counter <= 55;
+                        return_state <= WRITE_BLOCK_INIT;
+                        state <= SEND_CMD;
+                ready_for_next_byte <= 1;
+                    end
+                    WRITE_BLOCK_INIT: begin
+                        cmd_mode <= 0;
+                        byte_counter <= WRITE_DATA_SIZE; 
+                        state <= WRITE_BLOCK_DATA;
+                        ready_for_next_byte <= 0;
+                    end
+                    WRITE_BLOCK_DATA: begin
+                        if (byte_counter == 0) begin
+                            state <= RECEIVE_BYTE_WAIT;
+                            return_state <= WRITE_BLOCK_WAIT;
                         end
                         else begin
-                            data_sig <= {data_sig[6:0], 1'b1};
-                            bit_counter <= bit_counter - 1;
-                        end;
-                    end;
-                    sclk_sig <= ~sclk_sig;
-                end
-                WRITE_BLOCK_WAIT: begin
-                    if (sclk_sig == 1) begin
-                        if (miso == 1) begin
-                            state <= IDLE;
-                            cmd_mode <= 1;
+                            if ((byte_counter == 2) || (byte_counter == 1)) begin
+                                data_sig <= 8'hFF;
+                            end
+                            else if (byte_counter == WRITE_DATA_SIZE) begin
+                                data_sig <= 8'hFE;
+                            end
+                            else begin
+                                data_sig <= din;
+                                ready_for_next_byte <= 1;
+                            end
+                            bit_counter <= 7;
+                            state <= WRITE_BLOCK_BYTE;
+                            byte_counter <= byte_counter - 1;
                         end
                     end
-                    sclk_sig = ~sclk_sig;
-                end
-            endcase
+                    WRITE_BLOCK_BYTE: begin
+                        if (sclk_sig == 1) begin
+                            if (bit_counter == 0) begin
+                                state <= WRITE_BLOCK_DATA;
+                                ready_for_next_byte <= 0;
+                            end
+                            else begin
+                                data_sig <= {data_sig[6:0], 1'b1};
+                                bit_counter <= bit_counter - 1;
+                            end;
+                        end;
+                        sclk_sig <= ~sclk_sig;
+                    end
+                    WRITE_BLOCK_WAIT: begin
+                        if (sclk_sig == 1) begin
+                            if (miso == 1) begin
+                                state <= IDLE;
+                                cmd_mode <= 1;
+                            end
+                        end
+                        sclk_sig = ~sclk_sig;
+                    end
+                endcase
+            end
         end
     end
 
